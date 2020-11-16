fb7a92985cb66df59212a7be03dfd0d8dd512569
Barakaian <ianb.makale@gmail.com>
2020-11-16 20:27:24 +0300

    add the class Profile

---
+++ src/app/profile.ts
@@ -0,0 +1,9 @@
+export class Profile{
+    name: string;
+    followers: number;
+    following: number;
+    avatar_url: any;
+    // start(){
+    //   console.log("This is my profile!");
+    // }
+  }
