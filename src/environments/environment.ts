d85d61c908ac0c619f73786f41237b07fe1bf46a
Barakaian <ianb.makale@gmail.com>
2020-11-16 20:33:38 +0300

    add the changes in the file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+// import { environments } from '../environments/environment';
 export const environment = {
-  production: false
+  production: false,
+  prodt: "4ca2758e99eecbac783f55d080c07142c8decc7d"
+
 };
 
 /*
